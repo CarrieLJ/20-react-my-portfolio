4819f1ccd494e43ca3c5d19ff0c6e5c3644c6db4
Mona Lisa <cjensen@netechnical.com>
2023-03-22 15:53:33 -0500

    installed react, created component files, added code to App.js

---
+++ 00-app/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="portfolio-app">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
