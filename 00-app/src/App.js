b9a55fbf57b3957ef63072dd4535c93b304de7bb
Mona Lisa <cjensen@netechnical.com>
2023-03-27 16:14:12 -0500

    added useState to react to no longer get handlePageChange function as an error

--- 00-app/src/App.js
+++ 00-app/src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Project from "./components/Project";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-// import Container from "./components/container";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
   return (
     <div>
-      <Navigation />
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
       <Header />
       <Project />
       <Footer />
