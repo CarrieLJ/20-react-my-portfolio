b5836d8853d26bca61ca46e07af4370938d687a6
Mona Lisa <cjensen@netechnical.com>
2023-03-27 08:35:52 -0500

    removed uneccessary code and files, created css files, added code into components

--- 00-app/src/components/Navbar.js
+++ 00-app/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../styles/Header.css';
+import '../styles/Navbar.css';
 
-// By importing the Header.css file, it is added to the DOM whenever this component loads
+// By importing the Navbar.css file, it is added to the DOM whenever this component loads
 
 // We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
 // Unlike regular HTML, a JSX style property must be an object instead of a string
@@ -9,22 +9,20 @@ import '../styles/Header.css';
 // Non quoted values default to "pixels", e.g. height, margin, padding
 
 const styles = {
-  headerStyle: {
-    background: 'red',
-  },
-  headingStyle: {
-    fontSize: '100px',
+  navbarStyle: {
+    background: 'green',
+    justifyContent: 'flex-end',
   },
 };
 
-// We use JSX curly braces to evaluate the style object
+// We use JSX curly braces to evaluate the style object on the JSX tag
 
-function Header() {
+function Navbar() {
   return (
-    <header style={styles.headerStyle} className="header">
-      <h1 style={styles.headingStyle}>Welcome</h1>
-    </header>
+    <nav style={styles.navbarStyle} className="navbar">
+      <a href="/">Welcome</a>
+    </nav>
   );
 }
 
-export default Header;
+export default Navbar;
