b0e1cab478368fc728229e81a42395669807d923
Mona Lisa <cjensen@netechnical.com>
2023-04-03 15:20:39 -0500

    moved my snips of projects into a new folder called work, trying to get my home page to render what I want, added code into projectCard, and updated css for footer and project

---
+++ 00-app/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/Home.css';
+//import a picture?
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Get to know me!</h1>
+        <img src={homePic} alt="carrie" width="300"></img>  
+      </div>
+    );
+  };
+  
--- 00-app/src/components/pages/projectCard.js
+++ 00-app/src/components/pages/projectCard.js
@@ -1,18 +1,40 @@
 import React from "react";
+import Card from 'react.bootstrapCard';
 
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
-function ProjectCard(props) {
-  return (
-    <ul className="list-group">
-      {/* Here we map over each project item and return a new array of `li` elements that contains the project name */}
-      {/* When using map you must provide a unique key attribute to each project. Ours is `project.id` */}
-      {props.projects.map(project => (
-        <li className="list-group-project" key={project.id}>
-          {project.name}
-        </li>
-      ))}
-    </ul>
-  );
+function ProjectCard({ project }) {
+  const workCard = { padding: '5px', margin: '5px' };
+  const linkStyle = { color: 'black' };
+  const { title, tech, code, link } = project;
+
+  // return (
+  //   <div className="project" key={title} style={workCard}>
+  //     <Card style=({ width: '18rcm', borderRadius: '25px' }) >
+  //       <Card.Img style=[{ width: '18rcm' }] variant="top" src=(require{".../assets/work/$(title).jpg"}) alt=[title] />
+  //       <Card.Body>
+  //         <Card.Title>
+  //           <Card.Link href={code}><i className="fab fa-github"></i></Card.Link>
+  //           <Card.Link style={linkStyle} href={link}>{title}</Card.Link>
+  //         </Card.Title>
+  //       </Card.Body>
+  //       <Card.Body>
+  //         <Card.Text>(tech)</Card.Text>
+  //       </Card.Body>
+  //     </Card>
+  //   </div>
+
+
+  
+    // <ul className="list-group">
+    //   {/* Here we map over each project item and return a new array of `li` elements that contains the project name */}
+    //   {/* When using map you must provide a unique key attribute to each project. Ours is `project.id` */}
+    //   {props.projects.map(project => (
+    //     <li className="list-group-project" key={project.id}>
+    //       {project.name}
+    //     </li>
+    //   ))}
+    // </ul>
+  // );
 }
 
 export default ProjectCard;
